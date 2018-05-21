0e1b0e38b0e5945bdf86937935d2f44bce4744ce
tsubasa1218 <juan.suaza@correounivalle.edu.co>
2018-05-20 23:22:32 -0500

    New implementation test

--- main.py
+++ main.py
@@ -1,53 +1,111 @@
 from ortools.linear_solver import pywraplp
+from ortools.constraint_solver import pywarpcp
+from ortools.constraint_solver import solver_parameters_pb2
+import read_files 
+import os
 
-
+PATH_TO_FILE = os.path.dirname(os.path.realpath(__file__)) + "\\test.txt"
 #Input vars
-U=[[5, 6, 3, 5,6, 8, 2, 7, 4], [4, 1, 5, 5, 6, 7, 2, 7, 3], [9, 2, 3, 5, 1, 8, 3, 4, 1],[ 8, 4, 1, 3, 7, 4, 2, 7, 3]]
-T=[3, 2, 1, 3]
 
-m= 9
-cantidadParcelas=4
+def get_data():
 
-solver = pywraplp.Solver('EjemploLineal',pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    input_data_dict = read_files.read_file(PATH_TO_FILE)
 
-X=[]
-C=[]
+    U = input_data_dict["U"]#[[5, 6, 3, 5,6, 8, 2, 7, 4], [4, 1, 5, 5, 6, 7, 2, 7, 3], [9, 2, 3, 5, 1, 8, 3, 4, 1],[ 8, 4, 1, 3, 7, 4, 2, 7, 3]]
+    T = input_data_dict["T"]#[3, 2, 1, 3]
+
+    m = input_data_dict["m"]#9
+    cantidadParcelas = input_data_dict["P"]#4
+
+    return U, T, m, cantidadParcelas
+
+def model(U, TC, m, cantidadParcelas):
+    parameters = pywrapcp.Solver.DefaultSolverParameters()
+    solver = pywrapcp.Solver("Test", parameters)
+
+    #Define the variables for the problem
+    X=[[]] * cantidadParcelas
+    C=[[]] * cantidadParcelas
+
+    for i in range(0, len(X)):
+        X[i] = solver.IntVar(0.0, solver.infinity(), 'X_'+str(i+1))
+        C[i] = solver.BoolVar('C_'+str(i+1))
+
+    #Constraints
+    for i in range(0, cantidadParcelas):
+        #solver.Add( X[i] <= m )
+        solver.Add( X[i] + TC[i] - 1 <= m)
+
+    #https://developers.google.com/optimization/mip/integer_opt_cp
+    '''
+    solver = pywraplp.Solver('EjemploLineal',pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
+    X=[[]] * cantidadParcelas
+    C=[[]] * cantidadParcelas
+    
+    #Define the variables for the problem
+    for i in range(0, len(X)):
+        X[i] = solver.IntVar(0.0, solver.infinity(), 'X_'+str(i+1))
+        C[i] = solver.BoolVar('C_'+str(i+1))
+
+    #Objective
+    objective = solver.Objective()
+    objective.SetMaximization()
+
+    #Constraints
+    constraints = [0] * cantidadParcelas
+    for i in range(0, cantidadParcelas):
+        pass
+        #constraints[i] = 
+
+    
+
+    X=[]
+    C=[]
+
+    for i in range(0,cantidadParcelas):
+        x=solver.IntVar(-solver.infinity(),solver.infinity(), 'x_'+str(i+1))
+        c=solver.BoolVar('C_'+str(i+1))
+        X.append(x)
+        C.append(c)
+
+    #Restrictions
+
+    for i in range(0,cantidadParcelas):
+        restriction=solver.Constraint(0, m+T[i])
+        restriction.SetCoefficient(X[i],1)
 
 
-for i in range(0,cantidadParcelas):
-	x=solver.IntVar(-solver.infinity(),solver.infinity(), 'x_'+str(i+1))
-	c=solver.BoolVar('C_'+str(i+1))
-	X.append(x)
-	C.append(c)
+    #Objective function
+    objective = solver.Objective()
+
+    
+    for i in range(0,cantidadParcelas):
+        coefficient=0
+    for j in range(int(X[i].solution_value()),int(X[i].solution_value())+T[i]+1):
+        coefficient+=U[i][j]
+    objective.SetCoefficient(C[i],coefficient)
+    '''
+    return solver
 
 
-#Restrictions
-
-for i in range(0,cantidadParcelas):
-	restriction=solver.Constraint(0, m+T[i])
-	restriction.SetCoefficient(X[i],1)
-
-
-#Objective function
-objective = solver.Objective()
+def solve(solver):
+    solver.Solve();
+    
+    for i in range(0,cantidadParcelas):
+        print('X',str(i),' =',round(X[i].solution_value()))
 
 def dump(obj):
   for attr in dir(obj):
     print("obj.%s = %r" % (attr, getattr(obj, attr)))
 
 
+def main():
+    U, T, m, P = get_data()
+    solver = model(U, T, m, P)
+    solve(solver)
 
+if __name__ == "__main__":
+    main()
 
-
-for i in range(0,cantidadParcelas):
-	coefficient=0
-	for j in range(int(X[i].solution_value()),int(X[i].solution_value())+T[i]+1):
-		coefficient+=U[i][j]
-	objective.SetCoefficient(C[i],coefficient)
-
-
-solver.Solve()
-
-for i in range(0,cantidadParcelas):
-	print('X',str(i),' =',round(X[i].solution_value()))
 
--- read_files.py
+++ read_files.py
@@ -4,7 +4,7 @@ def read_file(path):
     with open(path) as file:
         dict = {
             "m" : 0,
-            "TC" : [], #Duration of each Parcela
+            "T" : [], #Duration of each Parcela
             "P" : 0,
             "U" : []} #Utilities matrix
         lineCounter = 0
@@ -12,7 +12,7 @@ def read_file(path):
             if(lineCounter == 0):
                 dict["m"] = int(line)
             elif(lineCounter == 1):
-                dict["TC"] = str_to_list(line)
+                dict["T"] = str_to_list(line)
             elif(lineCounter == 2):
                 dict["P"] = int(line)
             else:
@@ -29,7 +29,4 @@ def str_to_list(string):
 
     return list
 
-if __name__ == "__main__":
-    path_to_file = os.path.dirname(os.path.realpath(__file__)) + "\\test.txt"
-    print(path_to_file)
-    read_file(path_to_file)
+
